b75b442e246aadc55e812f57026d9e863da5aa54
BrianScalabrine <39539427+BrianScalabrine@users.noreply.github.com>
2018-05-27 00:17:54 -0700

    deploy

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -105,7 +105,7 @@ class SignIn extends Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('http://localhost:3000/signin', {
+        fetch('https://safe-springs-20265.herokuapp.com/signin', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
